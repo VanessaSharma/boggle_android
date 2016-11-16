1e6989625728caa09637212a495d85fb6a557f52
Eric Krause and Vanessa Palacios Sharma <eric+vanessa@>
2016-11-16 09:17:51 -0800

    Add toast to imageview.

--- app/src/main/java/com/epicodus/bogglesoltaire/MainActivity.java
+++ app/src/main/java/com/epicodus/bogglesoltaire/MainActivity.java
@@ -2,12 +2,25 @@ package com.epicodus.bogglesoltaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class atom MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+public class MainActivity extends AppCompatActivity {
+        private Button mPlayBoggleButton;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            mPlayBoggleButton = (Button) findViewById(R.id.playBoggleButton);
+            mPlayBoggleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Ready to play!", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
-}
