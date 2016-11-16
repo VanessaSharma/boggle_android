d8c98f7a0a5a131d0616d3c6233f7c69ba95d747
Eric Krause and Vanessa Palacios Sharma <eric+vanessa@>
2016-11-16 13:15:05 -0800

    Add array for random letter generator.

--- app/src/main/java/com/epicodus/bogglesoltaire/BoggleActivity.java
+++ app/src/main/java/com/epicodus/bogglesoltaire/BoggleActivity.java
@@ -1,15 +1,123 @@
+//package com.epicodus.bogglesoltaire;
+//
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Arrays;
+//import java.util.Random;
+//
+//public class Letter {
+//    private String letter;
+//
+//    public Letter(String letter) {
+//        this.letter = letter;
+//    }
+//
+//    public String getLetter() {
+//        return letter;
+//    }
+//}
+//
+//public class BoggleActivity extends AppCompatActivity {
+//    private String[] letters = { "a", "b", "c", "d", "e", "f", "g",
+//            "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+//            "u", "v", "w", "x", "y", "z" };
+//    private ArrayList<Letter> alphebet = new ArrayList<Letter>();
+//    private ArrayList<Letter> randomLetters = new ArrayList<Letter>();
+//
+//    public ArrayList<Letter> getAlphebet() {
+//        int value = 1;
+//        for (String letter: letters) {
+//            Letter letter = new Letter(letter);
+//            value ++;
+//            alphebet.add(letter);
+//        }
+//        return alphebet;
+//    }
+//
+//    public ArrayList<Letter> getRandomLetters() {
+//        for (int i = 0; i < 8; i++) {
+//            Random random = new Random();
+//            int randomNumber = random.nextInt(alphebet.size());
+//            String chosen = alphebet.get(randomNumber);
+//            randomLetters.add(chosen);
+//            alphebet.remove(chosen);
+//        }
+//        return randomLetters;
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_boggle);
+//        Intent intent = getIntent();
+//    }
+//}
+
 package com.epicodus.bogglesoltaire;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class BoggleActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Bind(R.id.start) EditText mStart;
+    @Bind(R.id.lettersGrid) GridView mLettersGrid;
+    @Bind(R.id.generateButton) Button mGenerateButton;
+    private String[] letters = new String[] {"A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
+    private ArrayList<String> randomLetters = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boggle);
+
+        ButterKnife.bind(this);
+
+
+
+        mGenerateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                do {
+                    Random random = new Random();
+                    int randomNum = random.nextInt(letters.length);
+                    String theLetter = letters[randomNum];
+                    Log.d(TAG, theLetter);
+                    randomLetters.add(theLetter);
+                    ArrayAdapter adapter = new ArrayAdapter(BoggleActivity.this, android.R.layout.simple_list_item_1, randomLetters);
+                    mLettersGrid.setAdapter(adapter);
+                }while (randomLetters.size() <8);
+            }
+        });
+
+
+
         Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+        mStart.setText("Start playing " + username);
     }
 }
--- app/src/main/java/com/epicodus/bogglesoltaire/MainActivity.java
+++ app/src/main/java/com/epicodus/bogglesoltaire/MainActivity.java
@@ -1,3 +1,31 @@
+
+//public class MainActivity extends AppCompatActivity {
+//    public static final String TAG = MainActivity.class.getSimpleName();
+//    private Button mPlayBoggleButton;
+//    private EditText mRollDiceEditText;
+//    private TextView mBoggleSoltaireTextView;
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_main);
+//            mRollDiceEditText = (EditText) findViewById(R.id.rollDiceEditText);
+//            mPlayBoggleButton = (Button) findViewById(R.id.playBoggleButton);
+//            mBoggleSoltaireTextView = (TextView) findViewById(R.id.boggleSolitaireTextView);
+//            Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-sans/ostrich-regular.ttf");
+//            mBoggleSoltaireTextView.setTypeface(ostrichFont);
+//            mPlayBoggleButton.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    String location = mRollDiceEditText.getText().toString();
+//                    Log.d(TAG, location);
+//                    Intent intent = new Intent(MainActivity.this, BoggleActivity.class);
+//                    startActivity(intent);
+//                }
+//            });
+//        }
+//    }
+
 package com.epicodus.bogglesoltaire;
 
 import android.content.Intent;
@@ -10,30 +38,37 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mPlayBoggleButton;
-    private EditText mRollDiceEditText;
-    private TextView mBoggleSoltaireTextView;
+    @Bind(R.id.username) EditText mUsername;
+    @Bind(R.id.playButton) Button mPlayButton;
+    @Bind(R.id.boggleSoltaire) TextView mBoggleSoltaire;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            mRollDiceEditText = (EditText) findViewById(R.id.rollDiceEditText);
-            mPlayBoggleButton = (Button) findViewById(R.id.playBoggleButton);
-            mBoggleSoltaireTextView = (TextView) findViewById(R.id.boggleSolitaireTextView);
-            Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-sans/ostrich-regular.ttf");
-            mBoggleSoltaireTextView.setTypeface(ostrichFont);
-            mPlayBoggleButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String location = mRollDiceEditText.getText().toString();
-                    Log.d(TAG, location);
-                    Intent intent = new Intent(MainActivity.this, BoggleActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-sans/ostrich-regular.ttf");
+          mBoggleSoltaire.setTypeface(ostrichFont);
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = mUsername.getText().toString();
+                Log.d(TAG, username);
+
+                Intent intent = new Intent(MainActivity.this, BoggleActivity.class);
+                intent.putExtra("username", username);
+                startActivity(intent);
+            }
+        });
+
     }
+}
